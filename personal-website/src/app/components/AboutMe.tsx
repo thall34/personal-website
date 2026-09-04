04c41e48f803705573c7117556d192be31d5762d
Tyler <209157553+thall34@users.noreply.github.com>
2026-09-04 19:20:52 -0400

    Add missing link to blog page from about section

--- personal-website/src/app/components/AboutMe.tsx
+++ personal-website/src/app/components/AboutMe.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+import Link from 'next/link';
+
 function AboutMe(): React.JSX.Element {
     return (
         <section id='about-me' className="fly-in-right">
@@ -6,6 +10,7 @@ function AboutMe(): React.JSX.Element {
             <p>I have always had an interest in how computers work under the hood, and after doing some research I found a great online resource in the Odin Project.</p>
             <p>Over the past year and a half, I have went from not knowing anything about HTML to building complete full stack applications from scratch which is extremely rewarding.</p>
             <p>My goal is to bring my technical and creative expertise to any project/company I get to work with next.</p>
+            <Link href='/blogs'>If you'd like to read more, head over to my blogs page</Link>
         </section>
     )
 };
