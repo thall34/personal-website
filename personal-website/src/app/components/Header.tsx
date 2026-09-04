9e4a1e00477265a001e91bc0e307619aa6656270
Tyler <209157553+thall34@users.noreply.github.com>
2026-09-04 19:33:40 -0400

    Fix header for small screens

--- personal-website/src/app/components/Header.tsx
+++ personal-website/src/app/components/Header.tsx
@@ -12,13 +12,7 @@ export default function Header() {
                         <Link href='#projects'>Projects</Link>
                     </li>
                     <li>
-                        <Link href='#websites'>Websites</Link>
-                    </li>
-                    <li>
                         <Link href='#about-me'>About</Link>
-                    </li>
-                    <li>
-                        <Link href='#skills'>Skills</Link>
                     </li>
                     <li>
                         <Link href='#contact'>Contact</Link>
