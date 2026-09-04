ba49b76c58dd65881380aa0dcac29c8eb6f0c6fd
Tyler <209157553+thall34@users.noreply.github.com>
2026-09-04 16:35:15 -0400

    Update resume and add headshot to hero

--- personal-website/src/app/components/Hero.tsx
+++ personal-website/src/app/components/Hero.tsx
@@ -1,9 +1,14 @@
 function Hero(): React.JSX.Element {
     return (
         <section id="hero">
-            <h1>Hello! My name is <span className="highlight-text">Tyler Hall</span></h1>
-            <p>I am a junior web developer from Canada that specializes in full stack development</p>
-            <p>I would love to get in touch and help bring your projects to life!</p>
+            <div className="hero-text">
+                <h1>Hello! My name is <span className="highlight-text">Tyler Hall</span></h1>
+                <p>I am a junior web developer from Canada that specializes in full stack development</p>
+                <p>I would love to get in touch and help bring your projects to life!</p>
+            </div>
+            <div className="hero-image">
+                <img src="./images/headshot.jpeg" alt="Headshot Photo of Tyler" />
+            </div>
         </section>
     )
 };
